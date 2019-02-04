c6c85a6680789bb2d6779ed377e0848c3ae82884
Kevin Lee <kevin.code@kevinlee.io>
2019-02-05 01:22:43 +1100

    Fixed: Incorrect transaction order for PageHandler2 Fixed: Commas from amount

--- src/main/scala/io/kevinlee/pdf2excel/pagehandler/PageHandler2.scala
+++ src/main/scala/io/kevinlee/pdf2excel/pagehandler/PageHandler2.scala
@@ -32,12 +32,21 @@ case object PageHandler2 extends PageHandler[TransactionDoc] {
   def findTransactionStart(page: Seq[String]): Seq[String] = {
     val droppedLines1 = page.dropWhile(line => line =/= transactionStart)
     if (droppedLines1.length < 2) {
-      List.empty[String]
+      Vector.empty[String]
     } else {
       val lines = droppedLines1.drop(1)
       val found = lines.headOption.exists(_.trim.startsWith("Date"))
       if (found) lines else findTransactionStart(lines)
     }
+  }
+
+  private def decideYear(month: Int): Int = {
+    val now = LocalDate.now()
+    val year = now.getYear
+    if (month === 12 && now.getMonthOfYear =/= 12)
+      year - 1
+    else
+      year
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.Nothing", "org.wartremover.warts.Any"))
@@ -46,12 +55,13 @@ case object PageHandler2 extends PageHandler[TransactionDoc] {
     if (lines.isEmpty) {
       None
     } else {
-      val months = List("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec").zipWithIndex.map { case (x, i) => (x, i + 1) }.toMap
+      val months = Vector("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec").zipWithIndex.map { case (x, i) => (x, i + 1) }.toMap
 
       val monthsP = P(StringIn(months.keys.toSeq:_*))
 
       val date = P(digits.rep.! ~ spaces.rep ~ monthsP.!).map { case (day, month) =>
-        LocalDate.parse(s"${LocalDate.now().getYear}-${months(month)}-$day")
+        val monthValue = months(month)
+        LocalDate.parse(s"${decideYear(monthValue)}-$monthValue-$day")
       }
 
       val lineP =
@@ -72,20 +82,21 @@ case object PageHandler2 extends PageHandler[TransactionDoc] {
               } else {
                 val words = a.split("[\\s]+").map(_.trim)
                 val (details, Array(card, amount)) = words.splitAt(words.length - 2)
+                val filteredAmount = amount.replaceAllLiterally(",", "")
                 processLine(
                   xs,
-                  Transaction(
+                  acc :+ Transaction(
                     d,
                     d,
                     card,
                     details.mkString(" "),
                     BigDecimal(
-                      if (amount.endsWith("-"))
-                        s"-${amount.dropRight(1)}"
+                      if (filteredAmount.endsWith("-"))
+                        s"-${filteredAmount.dropRight(1)}"
                       else
-                        amount
+                        filteredAmount
                     )
-                  ) +: acc
+                  )
                 )
               }
             case Parsed.Failure(_, _, _) =>
@@ -94,7 +105,7 @@ case object PageHandler2 extends PageHandler[TransactionDoc] {
 
       }
 
-      val content = processLine(lines.drop(1), Nil)
+      val content = processLine(lines.drop(1), Vector.empty)
       Some(TransactionDoc(header, content))
     }
   }
