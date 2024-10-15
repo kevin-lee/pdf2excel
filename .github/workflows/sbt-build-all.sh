946b5ed601628c867768162314b5ef444d277d7e
Kevin Lee <kevin.code@kevinlee.io>
2024-10-15 23:23:17 +1100

    Add GitHub Actions build - build.yml

---
+++ .github/workflows/sbt-build-all.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+set -x
+
+if [ -z "$1" ]
+  then
+    echo "Missing parameters. Please enter the [Scala version]."
+    echo "sbt-build-all.sh 2.13.12"
+    exit 1
+else
+  : ${CURRENT_BRANCH_NAME:?"CURRENT_BRANCH_NAME is missing."}
+
+  scala_version=$1
+  echo "============================================"
+  echo "Build projects"
+  echo "--------------------------------------------"
+  echo ""
+  export SOURCE_DATE_EPOCH=$(date +%s)
+  echo "SOURCE_DATE_EPOCH=$SOURCE_DATE_EPOCH"
+
+  if [[ "$CURRENT_BRANCH_NAME" == "main" || "$CURRENT_BRANCH_NAME" == "release" ]]
+  then
+#    sbt -J-Xmx2048m ++${scala_version}! -v clean; coverage; test; coverageReport; coverageAggregate
+#    sbt -J-Xmx2048m ++${scala_version}! -v coveralls
+#    sbt -J-Xmx2048m ++${scala_version}! -v clean; packagedArtifacts
+    sbt \
+      -J-XX:MaxMetaspaceSize=1024m \
+      -J-Xmx2048m \
+      ++${scala_version}! \
+      -v \
+      clean \
+      test \
+      packagedArtifacts
+  else
+#    sbt -J-Xmx2048m ++${scala_version}! -v clean coverage test coverageReport coverageAggregate package
+#    sbt -J-Xmx2048m ++${scala_version}! -v coveralls
+    sbt \
+      -J-XX:MaxMetaspaceSize=1024m \
+      -J-Xmx2048m \
+      ++${scala_version}! \
+      -v \
+      clean \
+      test \
+      package
+  fi
+
+
+  echo "============================================"
+  echo "Building projects: Done"
+  echo "============================================"
+fi
